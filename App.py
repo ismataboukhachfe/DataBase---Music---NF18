fe0cfd7a735ebb431ea94a228d4ff0347693eefd
MathieuKoz <127348750+MathieuKoz@users.noreply.github.com>
2024-05-14 11:23:48 +0200

    Update App.py
    connexion

--- App.py
+++ App.py
@@ -1,3 +1,17 @@
 #Fichier pour  creer l'application python de manipulation de donnees 
 
 import psycopg2
+
+HOST ="tuxa.sme.utc"
+USER = "nf18p032"
+PASSWORD = "Fb9EyCL7x6mZ"
+DATABASE = "dbnf18p032"
+
+try :
+	conn = psycopg2.connect("host=%s dbname=%s user=%s password=%s" % (HOST, DATABASE, USER, PASSWORD))
+	print("Connexion réussie \n")
+    #conn.close()
+	
+except Exception as error:
+	print("Une exception s'est produite : ", error)
+	print("Type d'exception : ", type(error))
